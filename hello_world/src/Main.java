03a4f01f69cebff9d7154d73b567e974730e7366
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-18 21:09:24 +0530

    simplified

--- hello_world/src/Main.java
+++ hello_world/src/Main.java
@@ -1,22 +1,24 @@
 public class Main {
   public static void main(String[] args) {
     int num[][] = new int[3][4];
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 4; j++) {
-        num[i][j] = (int) (Math.random() * 10);
-      }
-    }
-    for (int i = 0; i < 3; i++) {
-      for (int j = 0; j < 4; j++) {
-        System.out.print(num[i][j] + " ");
+    // for (int i = 0; i < 3; i++) {
+    // for (int j = 0; j < 4; j++) {
+    // num[i][j] = (int) (Math.random() * 10);
+    // }
+    // }
+    for (int n[] : num) {
+      for (int m : n) {
+        m = (int) (Math.random() * 10);
+        System.out.print(m + "");
       }
       System.out.println();
     }
-    for (int n[] : num) {
-      for (int m : n) {
-        System.out.print(m + "");
-      }
-      System.out.println(n);
-    }
+
+    // for (int n[] : num) {
+    // for (int m : n) {
+    // System.out.print(m + "");
+    // }
+    // System.out.println();
+    // }
   }
 }

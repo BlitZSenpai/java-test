4af0722c8aef58ac5adc5f0ec27d8752eae6da31
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-20 16:37:42 +0530

    threads test

---
+++ hello_world/src/Thread2.java
@@ -0,0 +1,26 @@
+public class A extends Thread {
+  public void start() {
+    System.out.println("Thread A");
+    for (int i = 0; i <= 3; i++) {
+      System.out.println("Thread A:" + i);
+    }
+  }
+}
+
+public class B extends Thread {
+  public void start() {
+    System.out.println("Thread B");
+    for (int i = 0; i <= 3; i++) {
+      System.out.println("Thread B:" + i);
+    }
+  }
+}
+
+public class Thread2 {
+  public static void main(String[] args) {
+    A thread1 = new A();
+    B thread2 = new B();
+    thread1.start();
+    thread2.start();
+  }
+}

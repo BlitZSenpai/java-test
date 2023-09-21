748c3142c21914fe97056601adf738770247c7f4
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-21 19:24:17 +0530

    even no. between 1-100

---
+++ hello_world/src/EvenMain.java
@@ -0,0 +1,11 @@
+public class EvenMain {
+  public static void main(String[] args) {
+    int sum = 0;
+    for (int i = 0; i <= 100; i++) {
+      if (i % 2 == 0) {
+        sum = sum + i;
+      }
+    }
+    System.out.println("The Sum of Even numbers between 1-100 : " + sum);
+  }
+}

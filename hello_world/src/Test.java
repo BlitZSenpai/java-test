e6c50e9cade9a5d3ff1dace73130c235cdd7fd52
BlitZSenpai <122906171+BlitZSenpai@users.noreply.github.com>
2023-09-23 13:03:56 +0530

    vector

--- hello_world/src/Test.java
+++ hello_world/src/Test.java
@@ -1,16 +1,20 @@
+import java.util.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class Test {
   public static void main(String[] args) {
     int n = 5;
     List<Integer> arr1 = new ArrayList<Integer>(n);
-    List<Integer> arr2 = new ArrayList<Integer>(n);
+    List<Integer> arr2 = new Vector<Integer>(n);
     for (int i = 1; i <= n; i++) {
       arr1.add(i);
       arr2.add(i + 1);
     }
     System.out.println(arr1);
+    for (int a : arr1)
+      System.out.println(a);
     System.out.println(arr2);
+    for (int b : arr2)
+      System.out.println(b);
   }
 }
